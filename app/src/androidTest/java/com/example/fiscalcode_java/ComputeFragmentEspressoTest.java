77ccfa3b02e6db26f144f96b69aba99f2e1246d6
Marta Pancaldi <marta.panc@gmail.com>
2020-06-29 00:00:57 +0100

    Add locale in Espresso test; test error messages

--- app/src/androidTest/java/com/example/fiscalcode_java/ComputeFragmentEspressoTest.java
+++ app/src/androidTest/java/com/example/fiscalcode_java/ComputeFragmentEspressoTest.java
@@ -8,15 +8,20 @@ import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 
 import org.hamcrest.Matchers;
+import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.Locale;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.hasErrorText;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isNotChecked;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
@@ -25,6 +30,9 @@ import static org.hamcrest.Matchers.containsString;
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class ComputeFragmentEspressoTest {
+
+    @ClassRule
+    public static final ForceLocaleRule localeTestRule = new ForceLocaleRule(Locale.ITALY);
 
     @Rule
     public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
@@ -41,5 +49,33 @@ public class ComputeFragmentEspressoTest {
         onView(withId(R.id.pob_autocompleteTextView)).perform(typeText("Guastalla (RE)"));
         onView(withId(R.id.compute_button)).perform(click());
         onView(withId(R.id.fiscalCodeOutput)).check(matches(withText(containsString("PNCMRT95L52E253R"))));
+    }
+
+    @Test
+    public void testShowsEmptyValueErrorMessages() {
+        onView(withId(R.id.compute_fragment)).check(matches(isDisplayed()));
+        onView(withId(R.id.compute_button)).perform(click());
+        onView(withId(R.id.first_name)).check(matches(hasErrorText("Valore richiesto")));
+        onView(withId(R.id.last_name)).check(matches(hasErrorText("Valore richiesto")));
+        onView(withId(R.id.femaleRadioButton)).check(matches(isNotChecked()));
+        onView(withId(R.id.maleRadioButton)).check(matches(isNotChecked()));
+        onView(withId(R.id.dateOfBirth_editText)).check(matches(withText("")));
+        onView(withId(R.id.pob_autocompleteTextView)).check(matches(hasErrorText("Valore richiesto")));
+    }
+
+    @Test
+    public void testShowsInvalidInputErrorMessages() {
+        onView(withId(R.id.first_name)).perform(typeText("Marta$"));
+        onView(withId(R.id.last_name)).perform(typeText("Panca1di"));
+        onView(withId(R.id.dateOfBirth_editText)).perform(click());
+        onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(PickerActions.setDate(1899, 1, 1));
+        onView(withText("OK")).perform(click());
+        onView(withId(R.id.pob_autocompleteTextView)).perform(typeText("Guastalla"));
+        onView(withId(R.id.compute_button)).perform(click());
+
+        onView(withId(R.id.first_name)).check(matches(hasErrorText("Il valore inserito è invalido")));
+        onView(withId(R.id.last_name)).check(matches(hasErrorText("Il valore inserito è invalido")));
+        onView(withId(R.id.dateOfBirth_editText)).check(matches(withText("01/01/1899")));
+        onView(withId(R.id.pob_autocompleteTextView)).check(matches(hasErrorText("Il valore inserito è invalido")));
     }
 }
