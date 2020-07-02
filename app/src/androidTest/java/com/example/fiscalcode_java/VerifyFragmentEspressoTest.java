04ff94f14ea1155922035a1671f8113aa6cf02a4
Marta Pancaldi <marta.panc@gmail.com>
2020-07-02 23:06:56 +0100

    Remove unused imports

--- app/src/androidTest/java/com/example/fiscalcode_java/VerifyFragmentEspressoTest.java
+++ app/src/androidTest/java/com/example/fiscalcode_java/VerifyFragmentEspressoTest.java
@@ -1,18 +1,13 @@
 package com.example.fiscalcode_java;
 
-import android.view.View;
 import android.widget.DatePicker;
 
-import androidx.test.espresso.UiController;
-import androidx.test.espresso.ViewAction;
 import androidx.test.espresso.contrib.PickerActions;
-import androidx.test.espresso.contrib.ViewPagerActions;
 import androidx.test.espresso.matcher.RootMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 
-import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import org.junit.ClassRule;
 import org.junit.Rule;
@@ -24,7 +19,6 @@ import java.util.Locale;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.swipeLeft;
-import static androidx.test.espresso.action.ViewActions.swipeRight;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
