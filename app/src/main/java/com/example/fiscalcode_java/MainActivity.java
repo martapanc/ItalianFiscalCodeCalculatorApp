01850a39115909a9d8f446fc5c0481a31c077ede
Marta Pancaldi <marta.panc@gmail.com>
2020-07-22 00:01:40 +0100

    Add empty toolbar with menu

--- app/src/main/java/com/example/fiscalcode_java/MainActivity.java
+++ app/src/main/java/com/example/fiscalcode_java/MainActivity.java
@@ -1,21 +1,24 @@
 package com.example.fiscalcode_java;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 import com.example.fiscalcode_java.ui.main.SectionsPagerAdapter;
 import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    //TODO: add activity for info/docs
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.menu_toolbar);
+        setSupportActionBar(toolbar);
 
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager(), 1, this);
         ViewPager viewPager = findViewById(R.id.view_pager);
@@ -25,5 +28,9 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.setupWithViewPager(viewPager);
     }
 
-
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_items, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 }
