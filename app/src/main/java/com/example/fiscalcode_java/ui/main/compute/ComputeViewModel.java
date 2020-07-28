0954caf5735373d4c8fbf565cc3e605f9e32bb94
Marta Pancaldi <marta.panc@gmail.com>
2020-07-28 15:22:54 +0100

    Add info button for Place of birth

--- app/src/main/java/com/example/fiscalcode_java/ui/main/compute/ComputeViewModel.java
+++ app/src/main/java/com/example/fiscalcode_java/ui/main/compute/ComputeViewModel.java
@@ -7,16 +7,20 @@ import androidx.lifecycle.ViewModel;
 import com.example.fiscalcode_java.fiscalcode.utils.ReadTownListHelper;
 
 import java.io.IOException;
+import java.util.Locale;
 
 public class ComputeViewModel extends ViewModel {
 
     public static final String TOWNS_FILE = "data/comuni.json";
-    public static final String COUNTRIES_FILE = "data/countries_it.json";
+    public static final String COUNTRIES_FILE = "data/countries_en.json";
+    public static final String COUNTRIES_FILE_IT = "data/countries_it.json";
+
+    private final String LANGUAGE = Locale.getDefault().getLanguage();
 
     public String[] getPlaceList(Context context) throws IOException {
         return ReadTownListHelper.readTownNameList(
                 context.getAssets().open(TOWNS_FILE),
-                context.getAssets().open(COUNTRIES_FILE)
+                context.getAssets().open(LANGUAGE.equalsIgnoreCase("EN") ? COUNTRIES_FILE : COUNTRIES_FILE_IT)
         );
     }
 }
