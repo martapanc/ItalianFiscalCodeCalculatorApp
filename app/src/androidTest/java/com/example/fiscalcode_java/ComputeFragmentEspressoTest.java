180802f18277ce232e2077b9404ffa9f5ef3ec4d
Marta Pancaldi <marta.panc@gmail.com>
2020-07-29 21:49:00 +0100

    Add Compute reset Espresso test

--- app/src/androidTest/java/com/example/fiscalcode_java/ComputeFragmentEspressoTest.java
+++ app/src/androidTest/java/com/example/fiscalcode_java/ComputeFragmentEspressoTest.java
@@ -1,5 +1,6 @@
 package com.example.fiscalcode_java;
 
+import android.util.JsonReader;
 import android.widget.DatePicker;
 
 import androidx.test.espresso.contrib.PickerActions;
@@ -7,12 +8,26 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 
+import com.example.fiscalcode_java.fiscalcode.models.Town;
+import com.example.fiscalcode_java.fiscalcode.utils.ReadTownListHelper;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+
 import org.hamcrest.Matchers;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 import static androidx.test.espresso.Espresso.onView;
@@ -26,6 +41,7 @@ import static androidx.test.espresso.matcher.ViewMatchers.isNotChecked;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 import static com.example.fiscalcode_java.TestConstants.INVALID_DOB;
 import static com.example.fiscalcode_java.TestConstants.INVALID_INPUT;
 import static com.example.fiscalcode_java.TestConstants.INVALID_LAST_NAME;
@@ -43,6 +59,7 @@ import static com.example.fiscalcode_java.TestConstants.VALID_TOWN;
 import static com.example.fiscalcode_java.TestConstants.VALID_YEAR;
 import static org.apache.commons.lang3.StringUtils.EMPTY;
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.isEmptyOrNullString;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -55,15 +72,9 @@ public class ComputeFragmentEspressoTest {
     public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void assertFiscalCodeIsComputedSuccessfully() throws InterruptedException {
+    public void assertFiscalCodeIsComputedSuccessfully() {
         onView(withId(R.id.compute_fragment)).check(matches(isDisplayed()));
-        onView(withId(R.id.com_first_name_input)).perform(typeText(VALID_NAME));
-        onView(withId(R.id.com_last_name_input)).perform(typeText(VALID_LAST_NAME));
-        onView(withId(R.id.com_femaleRadioButton)).perform(click());
-        onView(withId(R.id.com_dob_input)).perform(click());
-        onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(PickerActions.setDate(VALID_YEAR, VALID_MONTH, VALID_DAY));
-        onView(withText(OK)).perform(click());
-        onView(withId(R.id.com_pob_input)).perform(typeText(VALID_TOWN)).perform(closeSoftKeyboard());
+        typeValidInput();
         onView(withId(R.id.compute_button)).perform(click());
         onView(withId(R.id.com_fiscalCodeOutput)).check(matches(withText(containsString(VALID_FISCAL_CODE))));
     }
@@ -72,12 +83,7 @@ public class ComputeFragmentEspressoTest {
     public void assertShowsEmptyValueErrorMessages() {
         onView(withId(R.id.compute_fragment)).check(matches(isDisplayed())).perform(closeSoftKeyboard());
         onView(withId(R.id.compute_button)).perform(click());
-        onView(withId(R.id.com_first_name_input)).check(matches(hasErrorText(REQUIRED_VALUE)));
-        onView(withId(R.id.com_last_name_input)).check(matches(hasErrorText(REQUIRED_VALUE)));
-        onView(withId(R.id.com_femaleRadioButton)).check(matches(isNotChecked()));
-        onView(withId(R.id.com_maleRadioButton)).check(matches(isNotChecked()));
-        onView(withId(R.id.com_dob_input)).check(matches(withText(EMPTY)));
-        onView(withId(R.id.com_pob_input)).check(matches(hasErrorText(REQUIRED_VALUE)));
+        checkAllFieldsHaveEmptyValueError();
     }
 
     @Test
@@ -94,5 +100,38 @@ public class ComputeFragmentEspressoTest {
         onView(withId(R.id.com_last_name_input)).check(matches(hasErrorText(INVALID_INPUT)));
         onView(withId(R.id.com_dob_input)).check(matches(withText(INVALID_DOB)));
         onView(withId(R.id.com_pob_input)).check(matches(hasErrorText(INVALID_INPUT)));
+    }
+
+    @Test
+    public void assertResetButtonResetsAllFields() {
+        onView(withId(R.id.compute_reset_button)).perform(click());
+        onView(withId(R.id.com_first_name_input)).check(matches(withText(isEmptyOrNullString())));
+
+        typeValidInput();
+        onView(withText(VALID_TOWN)).perform(click()).perform(closeSoftKeyboard());
+        onView(withId(R.id.compute_reset_button)).perform(click());
+        onView(withId(R.id.com_first_name_input)).check(matches(withText(isEmptyOrNullString())));
+
+        onView(withId(R.id.compute_button)).perform(click());
+        checkAllFieldsHaveEmptyValueError();
+    }
+
+    private void typeValidInput() {
+        onView(withId(R.id.com_first_name_input)).perform(typeText(VALID_NAME));
+        onView(withId(R.id.com_last_name_input)).perform(typeText(VALID_LAST_NAME));
+        onView(withId(R.id.com_femaleRadioButton)).perform(click());
+        onView(withId(R.id.com_dob_input)).perform(click());
+        onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(PickerActions.setDate(VALID_YEAR, VALID_MONTH, VALID_DAY));
+        onView(withText(OK)).perform(click());
+        onView(withId(R.id.com_pob_input)).perform(typeText(VALID_TOWN)).perform(closeSoftKeyboard());
+    }
+
+    private void checkAllFieldsHaveEmptyValueError() {
+        onView(withId(R.id.com_first_name_input)).check(matches(hasErrorText(REQUIRED_VALUE)));
+        onView(withId(R.id.com_last_name_input)).check(matches(hasErrorText(REQUIRED_VALUE)));
+        onView(withId(R.id.com_femaleRadioButton)).check(matches(isNotChecked()));
+        onView(withId(R.id.com_maleRadioButton)).check(matches(isNotChecked()));
+        onView(withId(R.id.com_dob_input)).check(matches(withText(EMPTY)));
+        onView(withId(R.id.com_pob_input)).check(matches(hasErrorText(REQUIRED_VALUE)));
     }
 }
