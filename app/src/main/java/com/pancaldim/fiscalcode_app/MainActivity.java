ee52bd4fca9e41534ab95cf56f7469c5a37959ca
Marta Pancaldi <marta.panc@gmail.com>
2020-09-26 22:32:48 +0100

    Tests for navigation layout

--- app/src/main/java/com/pancaldim/fiscalcode_app/MainActivity.java
+++ app/src/main/java/com/pancaldim/fiscalcode_app/MainActivity.java
@@ -7,12 +7,15 @@ import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.ui.AppBarConfiguration;
 import androidx.viewpager.widget.ViewPager;
 
-import com.pancaldim.fiscalcode_app.ui.main.SectionsPagerAdapter;
 import com.google.android.material.tabs.TabLayout;
+import com.pancaldim.fiscalcode_app.ui.main.SectionsPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +31,23 @@ public class MainActivity extends AppCompatActivity {
 
         TabLayout tabLayout = findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
+
+//        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+//        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.material_drawer_open, R.string.material_drawer_close);
+//        drawer.addDrawerListener(drawerToggle);
+//        drawerToggle.syncState();
+
+//        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+//        NavigationView navigationView = findViewById(R.id.nav_view);
+//        // Passing each menu ID as a set of Ids because each
+//        // menu should be considered as top level destinations.
+//        mAppBarConfiguration = new AppBarConfiguration.Builder(
+//                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+//                .setDrawerLayout(drawer)
+//                .build();
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+//        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+//        NavigationUI.setupWithNavController(navigationView, navController);
     }
 
     @Override
