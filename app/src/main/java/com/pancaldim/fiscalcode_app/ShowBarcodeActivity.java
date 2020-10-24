813df96a3952b58b1db07f21b3589eb6e02f8946
Marta Pancaldi <marta.panc@gmail.com>
2020-10-24 15:57:03 +0100

    Test Save to gallery function - tbc

--- app/src/main/java/com/pancaldim/fiscalcode_app/ShowBarcodeActivity.java
+++ app/src/main/java/com/pancaldim/fiscalcode_app/ShowBarcodeActivity.java
@@ -1,14 +1,24 @@
 package com.pancaldim.fiscalcode_app;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.snackbar.Snackbar;
 import com.leinardi.android.speeddial.SpeedDialActionItem;
 import com.leinardi.android.speeddial.SpeedDialView;
 import com.pancaldim.fiscalcode_app.barcode.BarcodeGeneratorUtils;
 import com.pancaldim.fiscalcode_app.fiscalcode.models.FiscalCodeData;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class ShowBarcodeActivity extends AppCompatActivity {
 
@@ -22,6 +32,8 @@ public class ShowBarcodeActivity extends AppCompatActivity {
         FiscalCodeData fiscalCodeData = (FiscalCodeData) getIntent().getSerializableExtra(FISCAL_CODE_DATA_EXTRA_KEY);
 
         if (fiscalCodeData != null) {
+            final String fiscalCode = fiscalCodeData.getFiscalCode();
+
             TextView firstNameTextView = findViewById(R.id.brc_first_name_text);
             firstNameTextView.setText(fiscalCodeData.getFirstNameCode());
 
@@ -38,53 +50,33 @@ public class ShowBarcodeActivity extends AppCompatActivity {
             pobTextView.setText(fiscalCodeData.getPlaceOfBirth());
 
             TextView fiscalCodeTextView = findViewById(R.id.brc_fc_text);
-            fiscalCodeTextView.setText(fiscalCodeData.getFiscalCode());
+            fiscalCodeTextView.setText(fiscalCode);
 
             ImageView barcodeImageView = findViewById(R.id.brc_barcodeOutput);
-            barcodeImageView.setImageBitmap(BarcodeGeneratorUtils.generateCode39BarcodeImage(fiscalCodeData.getFiscalCode()));
+            barcodeImageView.setImageBitmap(BarcodeGeneratorUtils.generateCode39BarcodeImage(fiscalCode));
+
+            setupSpeedDial(fiscalCode);
         }
-
-        setupSpeedDial();
-
-//        RelativeLayout relativeLayout = findViewById(R.id.view_show_barcode);
-//        Bitmap bitmap = Bitmap.createBitmap(relativeLayout.getWidth(), relativeLayout.getHeight(), Bitmap.Config.ARGB_8888);
-//
-//        String root = Arrays.toString(getExternalMediaDirs());
-//        File fiscalCodeSaveDir = new File(root + "/saved_fiscal_codes");
-//        fiscalCodeSaveDir.mkdirs();
-//        String fileName = fiscalCodeData.getFiscalCode() + ".png";
-//        File savedFile = new File(fiscalCodeSaveDir, fileName);
-//
-//        try {
-//            FileOutputStream fos = new FileOutputStream(savedFile);
-//            bitmap.compress(Bitmap.CompressFormat.PNG, 90, fos);
-//            fos.flush();
-//            fos.close();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-
     }
 
-    private void setupSpeedDial() {
+    private void setupSpeedDial(String fiscalCode) {
+        View root = findViewById(android.R.id.content).getRootView();
         SpeedDialView speedDialView = findViewById(R.id.brc_speedDial);
         final int color = getResources().getColor(R.color.colorAccent, null);
-        speedDialView.addActionItem(new SpeedDialActionItem.Builder(R.id.brc_copy, R.drawable.ic_content_copy_24px)
+        speedDialView.addActionItem(new SpeedDialActionItem.Builder(R.id.brc_save, R.drawable.ic_save_alt_24px)
                 .setFabBackgroundColor(color)
                 .create());
-        speedDialView.addActionItem(new SpeedDialActionItem.Builder(R.id.brc_send, R.drawable.ic_share_24px)
+        speedDialView.addActionItem(new SpeedDialActionItem.Builder(R.id.brc_share, R.drawable.ic_share_24px)
                 .setFabBackgroundColor(color)
                 .create());
-
-        TextView fiscalCode = findViewById(R.id.com_fiscalCodeOutput);
 
         speedDialView.setOnActionSelectedListener(actionItem -> {
             switch (actionItem.getId()) {
-                case R.id.brc_copy:
-                    copyFunction(fiscalCode.getText());
+                case R.id.brc_save:
+                    saveFunction(root, fiscalCode);
                     break;
-                case R.id.brc_send:
-                    shareFunction(fiscalCode.getText());
+                case R.id.brc_share:
+                    shareFunction(root, fiscalCode);
                     break;
                 default:
                     return false;
@@ -93,28 +85,40 @@ public class ShowBarcodeActivity extends AppCompatActivity {
         });
     }
 
-    private void copyFunction(CharSequence fiscalCode) {
-        String message;
-//        final Context context = requireContext();
-//
-//        ClipboardManager clipboard = (ClipboardManager) Objects.requireNonNull(context).getSystemService(Context.CLIPBOARD_SERVICE);
-//        ClipData clipData = ClipData.newPlainText("Fiscal code", fiscalCode);
-//        Objects.requireNonNull(clipboard).setPrimaryClip(clipData);
-//        message = String.format(context.getResources().getString(R.string.copied_to_clipboard), fiscalCode);
-//
-//        Snackbar.make(root, message, Snackbar.LENGTH_LONG)
-//                .setAction("action", null)
-//                .show();
+    private void saveFunction(View root, String fiscalCode) {
+        exportToGallery(fiscalCode);
+        Snackbar.make(root, "Saved to gallery", Snackbar.LENGTH_LONG)
+                .setAction("action", null)
+                .show();
     }
 
-    private void shareFunction(CharSequence fiscalCode) {
-//        Intent sendIntent = new Intent();
-//        sendIntent.setAction(Intent.ACTION_SEND);
-//        sendIntent.putExtra(Intent.EXTRA_TEXT, fiscalCode);
-//        sendIntent.setType("text/plain");
-//
-//        Intent shareIntent = Intent.createChooser(sendIntent, null);
-//        root.getContext().startActivity(shareIntent);
+    private void shareFunction(View root, CharSequence fiscalCode) {
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, fiscalCode);
+        sendIntent.setType("text/plain");
 
+        Intent shareIntent = Intent.createChooser(sendIntent, null);
+        root.getContext().startActivity(shareIntent);
+    }
+
+    private void exportToGallery(String fiscalCode) {
+        RelativeLayout relativeLayout = findViewById(R.id.fc_data_group);
+        Bitmap bitmap = Bitmap.createBitmap(relativeLayout.getWidth(), relativeLayout.getHeight(), Bitmap.Config.ARGB_8888);
+
+        String rootDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES).getAbsolutePath();
+        File fiscalCodeSaveDir = new File(rootDir + "/ComputeFiscalCode");
+        fiscalCodeSaveDir.mkdirs();
+        String fileName = fiscalCode + ".png";
+        File savedFile = new File(fiscalCodeSaveDir, fileName);
+
+        try {
+            FileOutputStream fos = new FileOutputStream(savedFile);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 90, fos);
+            fos.flush();
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
